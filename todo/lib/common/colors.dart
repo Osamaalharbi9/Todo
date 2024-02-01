6f5a12d63ecbb30c60a99c7a639841103cb3c288
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2024-02-01 20:43:04 +0300

    delete

---
+++ todo/lib/common/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+const Color kRedColor=Colors.red;
+Color kPurpleColor=Colors.purple.shade100;
+Color kBgColor=Colors.grey.shade300;
+Color kWhiteColor=Colors.grey.shade100;
+Color kBlackColor=Colors.black87;
--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:todo/pages/home_page.dart';
+import 'package:todo/screens/home_page.dart';
 
 void main(){
-  runApp(const MyApp());
 
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(theme: ThemeData(primarySwatch: Colors.grey),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.grey),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),);
+      home: const HomePage(),
+    );
   }
 }
